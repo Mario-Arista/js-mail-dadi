e04a7694d68e5e58ecf15adcce887692bd78253f
mario.arista <mario.arista.dev@gmail.com>
2024-02-01 17:18:14 +0100

    control emails don't working yet

--- arrayemails/script.js
+++ arrayemails/script.js
@@ -1,15 +1,33 @@
 // Aggiungo array con email
 
-const userMail = ["mario@tiscali.it", "giovanni@tiscali.it", "michele@tiscali.it","giovanni@tiscali.it"];
+const userArrayMails = ["mario@tiscali.it", "giovanni@tiscali.it", "michele@tiscali.it","giovanni@tiscali.it","elisa@tiscali.it", "martina@tiscali.it", "caterina@tiscali.it"];
 
+
+// Mi creo una costante per Button
+const buttonMail = document.getElementById("button-mail");
 
 // Mi creo una costante per Input 
-const buttonUsersMail = document.getElementById("mail-user");
+const mailUsers = document.getElementById("mail-user");
 
-// Ragionamento prima di farlo: credo che sia meglio 
-// creare prima l'evento al click e dentro alla funzione
-//creare o un for con dentro un if o un if direttamente
+// Mi creo una costante output
+const containerElement = document.querySelector(".outputs-user");
+
+// Creo un evento al click con dentro un if per capire 
+// corrispondenza input utente - elemento array
+
+buttonMail.addEventListener("click",
+
+    function() {
+        const newElement = document.createElement("span")
+        containerElement.append(newElement);
+
+        if (mailUsers.value != userArrayMails[0, 1, 2, 3, 4, 5, 6]) {
+            newElement.innerHTML = `ERRORE, la tua mail non è presente nel database`;
+    
+        } else {
+            newElement.innerHTML = `La tua mail è presente nel database. Per continuare accedi all'area riservata!`;
+        }
 
 
-
-
+    }
+)
