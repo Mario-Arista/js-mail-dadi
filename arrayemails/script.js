fe2aa7cf2f6c1f469333861b8309e019f7db2b5b
mario.arista <mario.arista.dev@gmail.com>
2024-02-01 16:39:27 +0100

    array + input + general thinking

---
+++ arrayemails/script.js
@@ -0,0 +1,15 @@
+// Aggiungo array con email
+
+const userMail = ["mario@tiscali.it", "giovanni@tiscali.it", "michele@tiscali.it","giovanni@tiscali.it"];
+
+
+// Mi creo una costante per Input 
+const buttonUsersMail = document.getElementById("mail-user");
+
+// Ragionamento prima di farlo: credo che sia meglio 
+// creare prima l'evento al click e dentro alla funzione
+//creare o un for con dentro un if o un if direttamente
+
+
+
+
