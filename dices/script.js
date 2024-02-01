cb579e69487a9d6c5b5c1c7377fdda376792c126
mario.arista <mario.arista.dev@gmail.com>
2024-02-01 15:33:12 +0100

    first attempt. Not working beacuse using newElement and append

--- dices/script.js
+++ dices/script.js
@@ -1,15 +1,42 @@
 // Creo costante per imput del 
 // lancio random per l'utente
 
-const userDiceRandomElement = document.getElementById("user-dice")
+const userButtonElement = document.getElementById("user-dice")
 
 // Creo costanti per generare numeri random 
 // per dadi
 
 // UTENTE
 const diceUser = Math.floor(Math.random() * 6) + 1;
-console.log(diceUser);
 
 // COMPUTER
 const diceComputer = Math.floor(Math.random() * 6) + 1;
-console.log(diceComputer);
+
+// Genero costante con OUTPUT
+
+const outputsElement = document.querySelector(".outputs");
+
+// creo output al click dell'input
+
+userButtonElement.addEventListener("click",
+    function() {
+
+        // creo nuovo elemento output utente
+        const newElement = document.createElement("div");
+
+        // Genero nuovo elemento con append
+        outputsElement.append(newElement);
+
+        newElement.innerHTML = diceUser;
+
+        // creo nuovo elemento output computer
+        const newTwoElement = document.createElement("div");
+
+        // Genero nuovo elemento con append
+        outputsElement.append(newTwoElement);
+        
+        newTwoElement.innerHTML = diceComputer;
+
+
+    }
+)
