4b1bf6213bdb7390642971ba36eb41cff0e507ba
mario.arista <mario.arista.dev@gmail.com>
2024-02-02 11:18:42 +0100

    both exericise work, without style

--- arrayemails/script.js
+++ arrayemails/script.js
@@ -8,36 +8,36 @@ const buttonMail = document.getElementById("button-mail");
 // Mi creo una costante per Input 
 const mailUsers = document.getElementById("mail-user");
 
-// Mi creo una costante output
-const containerElement = document.querySelector(".outputs-user");
+// Mi creo output
+const outputUser = document.querySelector("#result-user");
 
 // Usare booleana con for con dentro if
-
-let emailCorretta =false;
 
 buttonMail.addEventListener("click",
 
     function() {
 
-        const newElement = document.createElement("span")
-        containerElement.append(newElement);
+        let emailCorretta = false;
 
         for ( i=0; i < userArrayMails.length; i++) {
             
-            if (userArrayMails[i] == mailUsers.value) {
-                let emailCorretta = true;
+            if (userArrayMails[i] === mailUsers.value) {
+                emailCorretta = true;
 
+                
+            }
+
+            if (emailCorretta) {
+
+                outputUser.innerHTML = "Ci sono";
+
+            } else {
+                outputUser.innerHTML = "Non ci sono";
             }
              
         }
-
-        if (emailCorretta) {
-            newElement.innerHTML = "Ci sono";
-        } else {
-            newElement.innerHTML = "Ci Faccio";
-        }
        
-
     }
+
 )
 
